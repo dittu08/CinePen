82aa5b4133d96c67f819b4c08c287069d39bd006
inthedeepblue <135726563+inthedeepblue@users.noreply.github.com>
2023-06-19 18:20:12 +0900

    Update main.php

--- main.php
+++ main.php
@@ -330,7 +330,7 @@
         <h3 class="trending">Trending Now</h3>
         <div class="card" style="width: 25rem;">
             <div class="re-card-content">
-                <p class="re-card-title">What is Lorem Ipsum?</p>
+                <p class="re-card-title">가디언즈 오브 갤럭시3 리뷰</p>
                 <p class="re-card-text">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
